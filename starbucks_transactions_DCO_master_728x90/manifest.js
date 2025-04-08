1a44163dd3b92eec78ccf4327f2aa0db5610e545
Md. Ariful Houqe <ariful.houqe@sebpo.com>
2025-04-08 15:15:15 +0600

    728x90 added

---
+++ starbucks_transactions_DCO_master_728x90/manifest.js
@@ -0,0 +1,173 @@
+FT.manifest({
+  "filename": "index.html",
+  "width": 728,
+  "height": 90,
+  "clickTagCount": 2,
+  "richLoads": [
+    {
+      "name": "richload1",
+      "src": "starbucks_retail_single_DCO_master_728x90_RL"
+    }
+  ],
+  "instantAds": [
+    {
+      "name": "start_product1_offsetX_offsetY",
+      "type": "text",
+      "default": "0|0"
+    },
+    {
+      "name": "end_product1_offsetX_offsetY",
+      "type": "text",
+      "default": "0|0"
+    },
+    {
+      "name": "start_product2_offsetX_offsetY",
+      "type": "text",
+      "default": "0|0"
+    },
+    {
+      "name": "end_product2_offsetX_offsetY",
+      "type": "text",
+      "default": "0|0"
+    },
+    {
+      "name": "F1_headline_txt",
+      "type": "text",
+      "default": "Espresso Tradition"
+    },
+    {
+      "name": "F1_headlineTxt_size_hex_glowHex",
+      "type": "text",
+      "default": "25|#fff|transparent"
+    },
+    {
+      "name": "F1_headline1_txt",
+      "type": "text",
+      "default": ""
+    },
+    {
+      "name": "F1_headline1Txt_size_hex",
+      "type": "text",
+      "default": "19|#fff"
+    },
+    {
+      "name": "F1_headline2_txt",
+      "type": "text",
+      "default": ""
+    },
+    {
+      "name": "F1_headline2Txt_size_hex",
+      "type": "text",
+      "default": "19|#fff"
+    },
+    {
+      "name": "F1_subHeadline_txt",
+      "type": "text",
+      "default": ""
+    },
+    {
+      "name": "F1_subHeadline_size_hex",
+      "type": "text",
+      "default": "19|#fff"
+    },
+    {
+      "name": "F2_headline1_txt",
+      "type": "text",
+      "default": "Caramel Macchiato"
+    },
+    {
+      "name": "F2_headline1Txt_size_hex",
+      "type": "text",
+      "default": "19|#fff"
+    },
+    {
+      "name": "F2_headline2_txt",
+      "type": "text",
+      "default": ""
+    },
+    {
+      "name": "F2_headline2Txt_size_hex",
+      "type": "text",
+      "default": "19|#fff"
+    },
+    {
+      "name": "F2_subHeadline_txt",
+      "type": "text",
+      "default": ""
+    },
+    {
+      "name": "F2_subHeadline_size_hex",
+      "type": "text",
+      "default": "6|#fff"
+    },
+    {
+      "name": "F3_headline_txt",
+      "type": "text",
+      "default": ""
+    },
+    {
+      "name": "F3_headlineTxt_size_hex",
+      "type": "text",
+      "default": "25|#fff"
+    },
+    {
+      "name": "ctaTxt",
+      "type": "text",
+      "default": "ORDER NOW"
+    },
+    {
+      "name": "ctaTxt_size_hex_hexHov",
+      "type": "text",
+      "default": "11|#fff|#1e3932"
+    },
+    {
+      "name": "ctaBtn_hex_hexHov",
+      "type": "text",
+      "default": "#1e3932|#fff"
+    },
+    {
+      "name": "background_hex",
+      "type": "text",
+      "default": "#1e3932"
+    },
+    {
+      "name": "border_hex",
+      "type": "text",
+      "default": "#919191"
+    },
+    {
+      "name": "clickTag1_url",
+      "type": "text",
+      "default": "https://starbucks.app.link/pjGHX4jnAab?%243p=a_custom_745318880125383532&~customer_placement=[%FT_PLACEMENT_ID%]&~campaign=fy23falldco&~creative_name=icedcaramelmacchiato"
+    },
+    {
+      "name": "clickTag2_url",
+      "type": "text",
+      "default": "https://starbucks.app.link/pjGHX4jnAab?%243p=a_custom_745318880125383532&~customer_placement=[%FT_PLACEMENT_ID%]&~campaign=fy23falldco&~creative_name=icedcaramelmacchiato"
+    },
+    {
+      "name": "product1_img",
+      "type": "image",
+      "default": "CaramelMacchiato_green_728x90.png"
+    },
+    {
+      "name": "product2_img",
+      "type": "image",
+      "default": "blank.gif"
+    },
+    {
+      "name": "logo_img",
+      "type": "image",
+      "default": "starbucks_logo.svg"
+    },
+    {
+      "name": "background_img",
+      "type": "image",
+      "default": "blank.gif"
+    },
+    {
+      "name": "richload1",
+      "type": "richLoad"
+    }
+  ]
+});
